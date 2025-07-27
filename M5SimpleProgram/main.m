fd8ad6c8d8a43f90da282129160084eff4f0dfec
KonaCurrents <konacurrents@users.noreply.github.com>
2025-07-27 10:26:17 -0700

    Initial Commit

---
+++ M5SimpleProgram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  M5SimpleProgram
+//
+//  Created by Scott Moody on 7/27/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
